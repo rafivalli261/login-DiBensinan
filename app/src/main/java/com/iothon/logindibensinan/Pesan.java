78317a5365e4e22a6a412e8122beae4be732c7e5
rafivalli261 <valliansyah@gmail.com>
2022-12-09 21:13:10 +0700

    adding order menu & on back press

---
+++ app/src/main/java/com/iothon/logindibensinan/Pesan.java
@@ -0,0 +1,14 @@
+package com.iothon.logindibensinan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pesan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pesan);
+    }
+}
